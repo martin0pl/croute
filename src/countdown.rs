e0b0d9bdb9bdc862ad53bbd8a4b1cbcdb695f7c6
Martin <168760158+martin0pl@users.noreply.github.com>
2026-05-05 22:26:06 +0200

    Delete Option<> to date

--- src/countdown.rs
+++ src/countdown.rs
@@ -2,11 +2,11 @@ use chrono::{DateTime, Utc, Duration};
 
 pub struct Countdown {
     title: String,
-    date: Option<DateTime<Utc>>
+    date: DateTime<Utc>
 }
 
 impl Countdown {
-    pub fn new(title: String, date: Option<DateTime<Utc>>) -> Countdown {
+    pub fn new(title: String, date: DateTime<Utc>) -> Countdown {
         Self {
             title : title,
             date : date
