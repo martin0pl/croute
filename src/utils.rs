927aca27f9b9b4bb9cde34af5a5814dcddf41de0
Martin <168760158+martin0pl@users.noreply.github.com>
2026-05-06 08:39:01 +0200

    Translate error

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use chrono::{DateTime, Utc, TimeZone};
 pub fn str_to_datetime(date_str: String) -> DateTime<Utc> {
     let format = "%Y-%m-%d %H:%M:%S";
 
-    let naive_datetime = chrono::NaiveDateTime::parse_from_str(&date_str, format).expect("Format invalide");
+    let naive_datetime = chrono::NaiveDateTime::parse_from_str(&date_str, format).expect("Invalid format");
 
     let datetime_utc: DateTime<Utc> = Utc.from_local_datetime(&naive_datetime).unwrap();
 
